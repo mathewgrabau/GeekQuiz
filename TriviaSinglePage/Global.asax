c8dcd0954b5d08b3f8142e9b29ef36b3ad1e3502
Mathew Grabau <mgrabau@mathewgrabau.ca>
2017-10-31 23:50:38 -0500

    Add project files.

---
+++ TriviaSinglePage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TriviaSinglePage.MvcApplication" Language="C#" %>
---
+++ TriviaSinglePage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TriviaSinglePage.Startup))]
+namespace TriviaSinglePage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
